dec940ec342719aeac73420dfe29801aae8a7c7c
Eric Nguyen <ericnguyen.developper@gmail.com>
2022-06-18 12:33:46 +0200

    feat: add MVP share screen page

--- apps/controller/src/Routes.tsx
+++ apps/controller/src/Routes.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom"
 import HomeMenu from "@/views/HomeMenu"
+import SharingPage from "./views/SharingPage"
 
 const Routes = (): React.ReactElement => (
   <BrowserRouter>
     <Switch>
+      <Route path="/share" element={<SharingPage />} />
       <Route path="/" element={<HomeMenu />} />
     </Switch>
   </BrowserRouter>
---
+++ apps/controller/src/views/SharingPage.tsx
@@ -0,0 +1,29 @@
+import { Typography, Stack, Grid } from "@mui/material"
+import ViewContainer from "@/components/ViewContainer"
+import BaseHeader from "@/components/BaseHeader"
+import Footer from "@/components/Footer"
+
+const SharingPage = () => (
+  <ViewContainer Header={BaseHeader} Footer={Footer}>
+    <Stack
+      direction="column"
+      justifyContent="center"
+      sx={{
+        height: "100%",
+        mx: 8,
+        paddingBottom: 6,
+      }}
+    >
+      <Grid container spacing={8}>
+        <Grid item xs={7}>
+          <Typography variant="h3">
+            Pour partager votre écran sur la télévision, brancher le cable hdmi
+            à votre ordinateur.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Stack>
+  </ViewContainer>
+)
+
+export default SharingPage
