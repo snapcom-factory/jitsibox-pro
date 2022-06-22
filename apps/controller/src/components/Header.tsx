3a50703178e998231ce027ed3720754559341558
Eric Nguyen <ericnguyen.developper@gmail.com>
2022-06-22 15:43:07 +0200

    feat(controller): Update Header

--- apps/controller/src/components/Header.tsx
+++ apps/controller/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Grid, Stack, Typography } from "@mui/material"
+import MarianneLogo from "@/assets/Marianne.svg?component"
 
 export interface HeaderProps {
   middleContent?: React.ReactNode
@@ -11,7 +12,12 @@ const Header = ({
 }: HeaderProps): React.ReactElement => (
   <Grid container direction="row" alignItems="center">
     <Grid item xs>
-      <Typography>Header placeholder</Typography>
+      <Stack direction="column" sx={{ width: "20vh" }}>
+        <MarianneLogo />
+        <Typography variant="h6" sx={{ paddingTop: 1, lineHeight: 1.1 }}>
+          WEBCONFÉRENCE DE L&apos;ÉTAT
+        </Typography>
+      </Stack>
     </Grid>
     {middleContent && (
       <Grid item xs>
