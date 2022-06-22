e0d360661085d4799324279ec0b1bad6543aba2f
Eric Nguyen <ericnguyen.developper@gmail.com>
2022-06-22 15:47:34 +0200

    feat(controller): Update Footer

--- apps/controller/src/components/Footer.tsx
+++ apps/controller/src/components/Footer.tsx
@@ -2,7 +2,18 @@ import { Stack, Typography } from "@mui/material"
 
 const Footer = (): React.ReactElement => (
   <Stack direction="row" justifyContent="space-between" alignItems="flex-end">
-    <Typography>Footer placeholder</Typography>
+    <Stack
+      direction="column"
+      justifyContent="space-between"
+      alignItems="flex-start"
+    >
+      <Typography variant="subtitle2" sx={{ lineHeight: 1 }}>
+        Jitsi-Box
+      </Typography>
+      <Typography variant="subtitle2" sx={{ lineHeight: 1 }}>
+        Controller #1234
+      </Typography>
+    </Stack>
   </Stack>
 )
 
