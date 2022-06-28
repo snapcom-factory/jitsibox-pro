4b0c17e9f8fe26d436a357977afaf2f0ad2cde4d
Eric Nguyen <ericnguyen.developper@gmail.com>
2022-06-28 10:10:37 +0200

    feat(controller): add not found page

---
+++ apps/controller/src/views/NotFound.tsx
@@ -0,0 +1,30 @@
+import { Stack } from "@mui/material"
+import Typography from "@mui/material/Typography"
+import { Close } from "@mui/icons-material"
+import { socketEvents } from "@jitsi-box-pro/model"
+import { ActionButton, Footer, Header, ViewContainer } from "@/components"
+
+const NotFound = () => (
+  <ViewContainer header={<Header />} footer={<Footer />}>
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={16}
+      sx={{
+        height: "100%",
+        paddingBottom: 4,
+      }}
+    >
+      <Typography variant="h2">Page non trouvée</Typography>
+      <ActionButton
+        color="secondary"
+        text="Retourner au menu"
+        Icon={Close}
+        event={{ name: socketEvents.global.cancel }}
+      />
+    </Stack>
+  </ViewContainer>
+)
+
+export default NotFound
--- apps/controller/src/views/index.ts
+++ apps/controller/src/views/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeMenu } from "@/views/HomeMenu"
 export { default as SharingPage } from "@/views/SharingPage"
 export { default as MeetingPage } from "@/views/MeetingPage"
+export { default as NotFound } from "@/views/NotFound"
