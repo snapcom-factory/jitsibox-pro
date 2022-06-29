2188b53ecf9a70362667f9dbe967668454141a89
Téo Chaillou <teo.chaillou@student-cs.fr>
2022-06-29 11:09:31 +0200

    fix: Make the keyboard accept a setValue prop

--- apps/controller/src/components/CustomKeyboard/CustomKeyboard.tsx
+++ apps/controller/src/components/CustomKeyboard/CustomKeyboard.tsx
@@ -3,9 +3,12 @@ import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
 import "./CustomKeyboard.css";
 
-const CustomKeyboard = () : React.ReactElement => {
+interface KeyboardProps {
+  setValue: (value: string) => void;
+}
+
+const CustomKeyboard = ({ setValue } : KeyboardProps) : React.ReactElement => {
   const [currentLayout, setCurrentLayout] = useState<string>('numpad');
-  const [value, setValue] = useState<string>('');
 
   const alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
 
@@ -21,7 +24,6 @@ const CustomKeyboard = () : React.ReactElement => {
 
   return (
     <div>
-      <input className='input' value={value} style={{ width: '100vw', height: '5vh', fontSize: '2em' }}/>
       {currentLayout === 'numpad' ? (
         <div className='side-panel' />
       ) : null}
