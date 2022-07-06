20cb9b896163ee6b2262dd6f0c3efd8e86589d17
Eric Nguyen <ericnguyen.developper@gmail.com>
2022-07-06 15:20:48 +0200

    feat(screen): add svgr compiling

--- apps/main-screen/vite.config.ts
+++ apps/main-screen/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import * as path from "path"
+import svgr from "@honkhonk/vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
