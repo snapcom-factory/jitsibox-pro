4af51d3b356c3aa3bd7f809a11bf85e4d12a8f5d
Eric Nguyen <ericnguyen.developper@gmail.com>
2022-07-07 15:11:17 +0200

    fix(controller) : remove events on sharing page

--- apps/controller/src/views/SharingPage.tsx
+++ apps/controller/src/views/SharingPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { Typography, Stack, Grid } from "@mui/material"
 import { Close } from "@mui/icons-material"
 import { socketEvents } from "@jitsi-box-pro/model"
@@ -9,107 +8,48 @@ import {
   ActionButton,
   ReturnToMenuButton,
 } from "@/components"
-import { useSocketListener } from "@/services/socket"
-import hdmiImageURL from "@/assets/hdmi.png"
-import { useSnackbarContext } from "@/services/snackbar"
 
-const SharingPage = () => {
-  const [isSharing, setIsSharing] = useState<boolean>(false);
-
-  const { openSnackbar } = useSnackbarContext()
-  useSocketListener(socketEvents.localSharing.start, () => {
-    setIsSharing(true)
-    openSnackbar(
-      "success",
-      { vertical: "bottom", horizontal: "center" },
-      "Écran partagé",
-      5000
-    )
-  })
-  useSocketListener(socketEvents.localSharing.stop, () => {
-    setIsSharing(false)
-    openSnackbar(
-      "warning",
-      { vertical: "bottom", horizontal: "center" },
-      "Câble HDMI débranché",
-      5000
-    )
-  })
-
-  return (
-    <ViewContainer
-      header={<Header endContent={<ReturnToMenuButton />} />}
-      footer={<Footer />}
+const SharingPage = () => (
+  <ViewContainer
+    header={<Header endContent={<ReturnToMenuButton />} />}
+    footer={<Footer />}
+  >
+    <Stack
+      direction="column"
+      justifyContent="center"
+      sx={{
+        height: "100%",
+        mx: 8,
+      }}
     >
-      <Stack
-        direction="column"
-        justifyContent="center"
-        sx={{
-          height: "100%",
-          mx: 8,
-        }}
-      >
-        <Grid container spacing={8}>
-          {!isSharing ? (
-            <>
-              <Grid item xs={7} sx={{ display: "flex", alignItems: "center" }}>
-                <Typography variant="h3">
-                  Pour partager votre écran sur la télévision, brancher le câble
-                  HDMI à votre ordinateur.
-                </Typography>
-              </Grid>
-              <Grid
-                item
-                xs={5}
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <img src={hdmiImageURL} alt="Câble HDMI" height={380} />
-              </Grid>
-            </>
-          ) : (
-            <>
-              <Grid item xs={9}>
-                <Typography variant="h3">
-                  Partage d&apos;écran activé.
-                </Typography>
-                <Typography
-                  variant="h3"
-                  sx={{
-                    paddingTop: 6,
-                  }}
-                >
-                  Pour arrêter le partage d&apos;écran, appuyer sur le bouton ou
-                  débrancher le câble.
-                </Typography>
-              </Grid>
-              <Grid
-                item
-                xs={3}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginTop: 2,
-                }}
-              >
-                <ActionButton
-                  text="Arrêter le partage"
-                  color="secondary"
-                  Icon={Close}
-                  event={{ name: socketEvents.global.cancel }}
-                />
-              </Grid>
-            </>
-          )}
+      <Grid container spacing={8}>
+        <Grid item xs={9} sx={{ display: "flex", alignItems: "center" }}>
+          <Typography variant="h3">
+            Pour partager votre ordinateur sur l&apos;écran, brancher le câble
+            HDMI à votre ordinateur.
+          </Typography>
         </Grid>
-      </Stack>
-    </ViewContainer>
-  )
-}
+        <Grid
+          item
+          xs={3}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 2,
+          }}
+        >
+          <ActionButton
+            text="Arrêter le partage"
+            color="secondary"
+            Icon={Close}
+            event={{ name: socketEvents.global.cancel }}
+          />
+        </Grid>
+      </Grid>
+    </Stack>
+  </ViewContainer>
+)
 
 export default SharingPage
