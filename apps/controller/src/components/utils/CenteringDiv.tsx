bdb3e2d481a03512080298a6ee720f8df55dd1e7
Eric Nguyen <ericnguyen.developper@gmail.com>
2022-07-15 18:22:42 +0200

    fix(controller): delete unnecessary component

--- apps/controller/src/components/utils/CenteringDiv.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, BoxProps, styled } from "@mui/material"
-
-const CenteringDiv = styled(Box)<BoxProps>(() => ({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-}))
-
-export default CenteringDiv
