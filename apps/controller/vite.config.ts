5fcb54d6caf229a0a7b2b3f7b92bc11bbb0e3cac
Georgy <georgy.grigoryev@gmail.com>
2022-09-16 09:14:22 -0500

    open port & host options

--- apps/controller/vite.config.ts
+++ apps/controller/vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
 })
+
--- apps/main-screen/vite.config.ts
+++ apps/main-screen/vite.config.ts
@@ -13,5 +13,6 @@ export default defineConfig({
   },
   server: {
     port: 3002,
+    host: "0.0.0.0",
   },
 })
